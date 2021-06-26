af9fd698b07db0f6d351088266bc842d33cb4427
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2021-06-26 20:11:55 -0300

    Update questao 7

--- T01 -  Álgebra Relacional/Questao-7-insertion.sql
+++ T01 -  Álgebra Relacional/Questao-7-insertion.sql
@@ -1,9 +1,9 @@
 
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (1, 'Lucas', 'Rua Augusto Monteiro', 'Caicó', 1500);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (2, 'José', 'Rua João Florêncio', 'Caicó', 2300);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (3, 'Maria', 'Rua Renato Dantas', 'Caicó', 6000);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (4, 'Lucas', 'Rua Dinarte Mariz', 'Jardim do Seridó', 2000);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (5, 'Joana', 'Rua José Eustáquio', 'Jardim do Seridó', 8000);
+INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (1, 'Lucas', 'Gustavo Diniz', 'Caicó', 15000);
+INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (2, 'José', 'Augusto Monteiro', 'Caicó', 2300);
+INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (3, 'Maria', 'Augusto  Monteiro', 'Caicó', 6000);
+INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (4, 'João', 'Dinarte Mariz', 'Jardim do Seridó', 2000);
+INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (5, 'Joana', 'José Eustáquio', 'Jardim do Seridó', 8000);
 
 INSERT INTO companhia (cod_companhia, nome_companhia, cidade_companhia) VALUES ('1', 'Soft Sell', 'Caicó');
 INSERT INTO companhia (cod_companhia, nome_companhia, cidade_companhia) VALUES ('2', 'Soft TI', 'Caicó');
@@ -18,5 +18,5 @@ INSERT INTO trabalha (cod_empregado, cod_companhia) VALUES (5, '4');
 
 INSERT INTO gerente (cod_empregado, cod_companhia) VALUES (4, '1');
 INSERT INTO gerente (cod_empregado, cod_companhia) VALUES (3, '2');
-INSERT INTO gerente (cod_empregado, cod_companhia) VALUES (2, '3');
 INSERT INTO gerente (cod_empregado, cod_companhia) VALUES (1, '4');
+INSERT INTO gerente (cod_empregado, cod_companhia) VALUES (3, '3');
