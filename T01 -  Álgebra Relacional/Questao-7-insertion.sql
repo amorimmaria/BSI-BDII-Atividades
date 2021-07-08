a3982b9dfb085a2df68208564b5102b2b218fd7b
Maria das Graças <44977553+amorimmaria@users.noreply.github.com>
2021-07-08 18:36:45 -0300

    Update Questao-7-insertion.sql

--- T01 -  Álgebra Relacional/Questao-7-insertion.sql
+++ T01 -  Álgebra Relacional/Questao-7-insertion.sql
@@ -1,9 +1,9 @@
 
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (1, 'Lucas', 'Gustavo Diniz', 'Caicó', 15000);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (2, 'José', 'Augusto Monteiro', 'Caicó', 2300);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (3, 'Maria', 'Augusto  Monteiro', 'Caicó', 6000);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (4, 'João', 'Dinarte Mariz', 'Jardim do Seridó', 2000);
-INSERT INTO EMPREGADO (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (5, 'Joana', 'José Eustáquio', 'Jardim do Seridó', 8000);
+INSERT INTO empregado (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (1, 'Lucas', 'Gustavo Diniz', 'Caicó', 15000);
+INSERT INTO empregado (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (2, 'José', 'Augusto Monteiro', 'Caicó', 2300);
+INSERT INTO empregado (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (3, 'Maria', 'Augusto  Monteiro', 'Caicó', 6000);
+INSERT INTO empregado (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (4, 'João', 'Dinarte Mariz', 'Jardim do Seridó', 2000);
+INSERT INTO empregado (cod_empregado, nome_empregado, rua, cidade, salario) VALUES (5, 'Joana', 'José Eustáquio', 'Jardim do Seridó', 8000);
 
 INSERT INTO companhia (cod_companhia, CNPJ, nome_companhia, cidade_companhia) VALUES ('1', '01234567895432' ,'Soft Sell', 'Caicó');
 INSERT INTO companhia (cod_companhia, CNPJ, nome_companhia, cidade_companhia) VALUES ('2', '54321789210014' ,'Soft TI', 'Caicó');
